b5ab7cf42d429ff1681526d19b4ce177238254fd
fadisassi <fadisassi@live.fr>
2020-06-18 16:00:11 +0200

    Ajout conditions

---
+++ conditions.py
@@ -0,0 +1,10 @@
+# if : si ...
+# elif sinon, si ...
+# else sinon (par defaut)
+
+#on verifie si un nombre est paire ou impaire
+nombre = 4
+if(nombre%2==0):
+    print("Nombre paire")
+else:
+    print("nombre impaire")
