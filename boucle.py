58e4814bc8c6d4e581678e218c08d83d2db6a857
fadisassi <fadisassi@live.fr>
2020-06-19 16:02:29 +0200

    Ajout de boucles

---
+++ boucle.py
@@ -0,0 +1,10 @@
+# il existe 2 type de boucle en python 
+# for ()
+# while () "tant que" la condition est vrai, fais...
+
+debut = 1 
+fin = 10
+
+while (debut<= fin):
+    print(debut)
+    debut = debut +1
