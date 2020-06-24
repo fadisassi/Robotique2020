aa8b6855efb843967e7b5bfd57c72846330d3784
fadisassi <fadisassi@live.fr>
2020-06-24 12:50:28 +0200

    Ajout boucle fort

--- boucle.py
+++ boucle.py
@@ -2,9 +2,31 @@
 # for ()
 # while () "tant que" la condition est vrai, fais...
 
-debut = 1 
+i = 1 # i pour increment par convention 
 fin = 10
 
-while (debut<= fin):
-    print(debut)
-    debut = debut +1
+while (i<= fin):
+    print("compteur =",i)
+    i = i +1
+
+texte = "Soudure"
+
+#for lettre in texte : 
+ #   print (lettre)
+
+nb_voyelles = 0
+nb_consonnes = 0
+
+for lettre in texte :
+    if lettre in "aeiouy" :
+        print(lettre)
+
+
+for lettre in texte : 
+    if lettre in "aeiouy" :
+        nb_voyelles = nb_voyelles +1 
+    else : 
+        nb_consonnes = nb_consonnes +1
+
+print("consonnes =" , nb_consonnes)
+print( "voyelles =", nb_voyelles)
